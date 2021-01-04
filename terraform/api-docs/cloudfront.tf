da03ee12d2a87d5f5de255896581958f5b11d7f2
Miguel Afonso <mafonso@gmail.com>
2021-01-04 18:01:53 +0000

    Create and validate a dedicated certificate for the CDN

--- terraform/api-docs/cloudfront.tf
+++ terraform/api-docs/cloudfront.tf
@@ -1,28 +1,29 @@
 resource "aws_cloudfront_distribution" "distribution" {
+  provider = aws.global
   origin {
-    domain_name = "tariff-api-dev.london.cloudapps.digital"
-    origin_id   = "origin-tariff-api-dev.london.cloudapps.digital"
+    domain_name = var.origin_endpoint
+    origin_id   = "origin-${var.origin_endpoint}"
 
     custom_origin_config {
-      http_port = 80
-      https_port = 443
+      http_port                = 80
+      https_port               = 443
       origin_keepalive_timeout = 5
-      origin_protocol_policy = "match-viewer"
-      origin_read_timeout = 30
-      origin_ssl_protocols = ["TLSv1.1","TLSv1.2"]
+      origin_protocol_policy   = "match-viewer"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1.1", "TLSv1.2"]
     }
   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  price_class = "PriceClass_100"
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_100"
 
-  aliases = [ "api-dev.trade-tariff.service.gov.uk"]
+  aliases = var.aliases
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "origin-tariff-api-dev.london.cloudapps.digital"
+    target_origin_id = "origin-${var.origin_endpoint}"
 
     forwarded_values {
       query_string = true
@@ -45,22 +46,21 @@ resource "aws_cloudfront_distribution" "distribution" {
 
   viewer_certificate {
     cloudfront_default_certificate = false
-    acm_certificate_arn = "arn:aws:acm:us-east-1:777015734912:certificate/59c22c7f-fbb5-4fc7-955f-00595c565a3b"
-    minimum_protocol_version = "TLSv1.1_2016"
-    ssl_support_method = "sni-only"
+    acm_certificate_arn            = aws_acm_certificate.cdn_certificate.arn
+    minimum_protocol_version       = "TLSv1.1_2016"
+    ssl_support_method             = "sni-only"
   }
-}
 
-
-data "aws_route53_zone" "selected" {
-  name         = "trade-tariff.service.gov.uk."
-  private_zone = false
+  depends_on = [
+    aws_acm_certificate.cdn_certificate,
+    aws_acm_certificate_validation.validation
+  ]
 }
 
 resource "aws_route53_record" "cdn-record" {
-  name = "api-dev.trade-tariff.service.gov.uk"
-  type = "CNAME"
-  ttl = 60
+  name    = var.fqdn
+  type    = "CNAME"
+  ttl     = 60
   records = [aws_cloudfront_distribution.distribution.domain_name]
   zone_id = data.aws_route53_zone.selected.id
 }
--- terraform/api-docs/main.tf
+++ terraform/api-docs/main.tf
@@ -5,3 +5,18 @@ terraform {
     region = "eu-west-2"
   }
 }
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "global"
+  region = "us-east-1"
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "trade-tariff.service.gov.uk."
+  private_zone = false
+}
